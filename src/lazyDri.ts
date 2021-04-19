6ed6a8b13f4e5be4698654722c38e2f24bf5732d
Vlad Limbean <limbeanvlad@gmail.com>
2021-04-19 19:20:18 +0300

    added wrap-around

--- src/lazyDri.ts
+++ src/lazyDri.ts
@@ -4,28 +4,16 @@ let currentIdx: number = 0;
 
 export function getSpot(numStaff: number, skip: number) {
     constructDRIList(numStaff);
-    while (dri.length !== numStaff) {
+    while (dri.length < numStaff) {
         exec(skip);
     }
     return dri;
 }
 
 function exec(skip: number) {
-    updateIndex(skip);
+    findNextIndex(skip);
     dri.push(staff[currentIdx]);
     remove();
-}
-
-function updateIndex(skip: number) {
-    currentIdx += skip - 1; // incremet spot
-    
-    if (currentIdx >= staff.length) {
-        currentIdx -= staff.length; // reset
-    }
-
-    if (staff.length === 1) {
-        currentIdx = 0; // end game
-    }
 }
 
 function constructDRIList(numStaff: number) {
@@ -38,11 +26,16 @@ function remove() {
     staff = staff.filter((_v, index) => currentIdx !== index);
 }
 
+function findNextIndex(skip) {
+    currentIdx += skip - 1; // incremet
+    while (currentIdx >= staff.length) {
+        currentIdx -= staff.length; // wrap around
+    }
+    if (staff.length === 1) { currentIdx = 0;} // end game
+}
+
 export function reset() {
     dri.splice(0, dri.length);
     staff.splice(0, staff.length);
     currentIdx = 0;
 }
-
-console.log('running!');
-getSpot(41, 3)
